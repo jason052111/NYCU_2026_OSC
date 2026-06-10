dd1b17447125a381e41e678c2ef286dd0429b54e
Dumbo <165047310+jason052111@users.noreply.github.com>
2026-06-11 04:09:02 +0800

    Delete Lab7 directory

--- Lab7/kernel/inc/ramfs.h
+++
@@ -1,8 +0,0 @@
-#ifndef RAMFS_H
-#define RAMFS_H
-
-#include "vfs.h"
-
-int ramfs_setup_mount(struct filesystem* fs, struct mount* mnt);
-
-#endif
--- Lab7/kernel/inc/tmpfs.h
+++
@@ -1,8 +0,0 @@
-#ifndef TMPFS_H
-#define TMPFS_H
-
-#include "vfs.h"
-
-int tmpfs_setup_mount(struct filesystem* fs, struct mount* mnt);
-
-#endif
